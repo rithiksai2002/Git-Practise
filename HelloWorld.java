5533d06c32f942396806cd5133a46866e39d4d61
Rithik Sai <68235912+rithiksai2002@users.noreply.github.com>
2024-09-02 15:36:15 +0530

    My first commit - added the Hello World java program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("This is a hello world program to execute this we are using JAVA");
+        System.out.println("This is done during the git working");
+    }
+}
