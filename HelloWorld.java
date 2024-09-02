4730eed4438ca6c43607d026f8b583353019f1ac
Rithik Sai <68235912+rithiksai2002@users.noreply.github.com>
2024-09-02 19:25:55 +0530

    modified the Hello World java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld
     public static void main(String[] args) {
         System.out.println("This is a hello world program to execute this we are using JAVA");
         System.out.println("This is done during the git working");
+        System.out.println("Added a new change");
     }
 }
